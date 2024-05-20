f935c1370036547d3bff1dad25390e07380ba172
jottachaves <joaocarlos0041@gmail.com>
2024-05-20 15:17:33 -0300

    dio-trilha-java-basico

---
+++ tipos-variaveis/src/AnatomiaDasClasses.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    99,9% das nossas classes iniciarão com public class;
+    toda classe precisa de nome, exemplo MinhaClasse;
+    O nome do arquivo deve ser identico ao nome da classe publica;
+    apos o nome, definir o corpo { }, onde iremos compor nossas classes com atributos e metodos.
+
+    //exemplo:
+public class MinhaClasse {
+    //corpo da classe
+
+    public static void main (String [] args){
+        //corpo do metodo main
+    }
+}    
+
+
+}
