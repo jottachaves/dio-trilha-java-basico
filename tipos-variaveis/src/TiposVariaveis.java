19cff7dc4cbe1c7e384de476ac640dc14a329903
jottachaves <joaocarlos0041@gmail.com>
2024-05-20 14:54:07 -0300

    update: 5

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá, pessoal.!");
+
+        String meuNome = "joao";//String representa texto
+        double salarioMinimo = 2500; //valor inteiro
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        numeroCurto = (short) numeroNormal; // Correção: Removida a redeclaração de numeroCurto
+
+        int numero = 5;
+        System.out.print(numero); // Saída: 5
+
+        final double VALOR_DE_PI = 3.14;//constante
     }
 }
